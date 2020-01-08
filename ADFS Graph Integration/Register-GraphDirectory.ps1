a2b78c730ed790f27cde7d3c64313e87c7f4c4ac
Rich Shillington <rishilli@microsoft.com>
2020-01-08 13:21:16 -0500

    Commit

---
+++ ADFS Graph Integration/Register-GraphDirectory.ps1
@@ -0,0 +1,45 @@
+﻿<#
+.SYNOPSIS
+    Script to register Graph Directory
+
+.DESCRIPTION
+    Use this to register the Graph Directory for Azure Stack ADFS implementations
+
+.PARAMETER CloudAdminUserName
+    Provide the User Name of the Cloud Admin.
+    Example: 'CloudAdmin@azurestack.local'
+
+.PARAMETER ADForestFQDN
+    Provide FQDN of the Active Directory Forest.
+    Example: blabla.contoso.com
+
+.PARAMETER PrivilegedEndpoints
+    Define list of Privileged Endpoints as an Array.
+    Example: @('10.0.0.1','10.0.0.2','10.0.0.3')
+
+.EXAMPLE
+    .\Register-GraphDirectory.ps1
+#>
+[CmdletBinding()]
+Param
+(
+    # Provide the User Name of the Cloud Admin.
+    # Example: 'CloudAdmin@azurestack.local'
+    [parameter(Mandatory=$false,HelpMessage='Provide the User Name of the Cloud Admin.')]
+    [String]$CloudAdminUserName = 'CloudAdmin@azurestack.local',
+
+    # Provide FQDN of the Active Directory Forest.
+    # Example: blabla.contoso.com
+    [parameter(Mandatory=$true,HelpMessage='Provide FQDN of the Active Directory Forest. Example: blabla.contoso.com')]
+    [String]$ADForestFQDN,
+
+    # Define list of Privileged Endpoints as an Array.
+    # Example: @("10.0.0.1","10.0.0.2","10.0.0.3")
+    [parameter(Mandatory=$false,HelpMessage='Define list of Privileged Endpoints as an Array. Example: @("10.0.0.1","10.0.0.2","10.0.0.3")')]
+    [Array]$PrivilegedEndpoints = @("10.0.0.1","10.0.0.2","10.0.0.3")
+)
+
+$CloudAdminCredential = Get-Credential -Credential $CloudAdminUserName
+$Session = New-PSSession -ComputerName (Get-Random -InputObject $PrivilegedEndpoints) -ConfigurationName PrivilegedEndpoint -Credential $CloudAdminCredential
+
+Invoke-Command $Session {Register-DirectoryService -CustomADGlobalCatalog $Using:ADForestFQDN}
---
+++ Offers Plans Quotas/Create-OffersPlansQuotasSample.ps1
@@ -0,0 +1,78 @@
+﻿<#
+.SYNOPSIS
+    Script to create Offers, Plans & Quotas
+
+.DESCRIPTION
+    Use this script as a template for creating Offers, Plans & Quotas using PowerShell
+
+.EXAMPLE
+    .\Create-OffersPlansQuotasSample.ps1
+#>
+
+# Enviornment Selection
+$Environments = Get-AzureRmEnvironment
+$Environment = $Environments | Out-GridView -Title "Please Select the Azure Stack Admin Enviornment." -PassThru
+
+#region Connect to Azure
+try
+{
+    Connect-AzureRmAccount -Environment $($Environment.Name) -ErrorAction 'Stop'
+}
+catch
+{
+    Write-Error -Message $_.Exception
+    break
+}
+
+try 
+{
+    $Subscriptions = Get-AzureRmSubscription
+    if ($Subscriptions.Count -gt '1')
+    {
+        $Subscription = $Subscriptions | Out-GridView -Title "Please Select the Default Provider Subscription." -PassThru
+        Select-AzureRmSubscription $Subscription
+    }
+}
+catch
+{
+    Write-Error -Message $_.Exception
+    break
+}
+
+$Location = Get-AzureRmLocation
+#endregion
+
+#region Standard IaaS Plan
+
+#region Create Quota
+$ComputeQuota = New-AzsComputeQuota -Name 'Standard_IaaS_Compute_Quota' -AvailabilitySetCount '10' `
+    -CoresCount '25' -VMScaleSetCount '10' `
+    -VirtualMachineCount '12' -StandardManagedDiskAndSnapshotSize '2048' `
+    -PremiumManagedDiskAndSnapshotSize '2048' -Location $Location.Location
+
+$NetworkQuota = New-AzsNetworkQuota -Name 'Standard_IaaS_Network_Quota' -MaxNicsPerSubscription '12' `
+    -MaxPublicIpsPerSubscription '24' -MaxVirtualNetworkGatewayConnectionsPerSubscription '0' `
+    -MaxVnetsPerSubscription '12' -MaxVirtualNetworkGatewaysPerSubscription '0' `
+    -MaxSecurityGroupsPerSubscription '24' -MaxLoadBalancersPerSubscription '12' `
+    -Location $Location.Location
+
+$StorageQuota = New-AzsStorageQuota -Name 'Standard_IaaS_Storage_Quota' -CapacityInGb '500' `
+    -NumberOfStorageAccounts '25' -Location $Location.Location
+
+$KeyVaultQuota = Get-AzsKeyVaultQuota
+#endregion
+
+#region Plan
+$Plan = New-AzsPlan -Name 'Standard_IaaS_Plan' -ResourceGroupName 'OfferAndPlans-RG' `
+    -DisplayName 'Standard IaaS Plan' -Description 'Standard IaaS Plan' `
+    -QuotaIds "$($ComputeQuota.Id)","$($NetworkQuota.Id)","$($StorageQuota.Id)","$($KeyVaultQuota.Id)" `
+    -Location $Location.Location
+#endregion
+
+#region Offer
+$Offer = New-AzsOffer -Name 'Standard_IaaS_Offer' -DisplayName 'Standard IaaS Offer' `
+    -ResourceGroupName 'OfferAndPlans-RG' -BasePlanIds $Plan.Id `
+    -Description 'Standard IaaS Offer' -State 'Private' -Location $Location.Location
+#endregion
+
+#endregion

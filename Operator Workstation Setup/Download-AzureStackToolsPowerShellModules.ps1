2469edd865de243651eb0afce4df136e27b26919
Rich Shillington <rishilli@microsoft.com>
2020-04-15 14:07:56 -0400

    Updated AzureStack module to 1.8.1 for 2002

--- Operator Workstation Setup/Download-AzureStackToolsPowerShellModules.ps1
+++ Operator Workstation Setup/Download-AzureStackToolsPowerShellModules.ps1
@@ -47,7 +47,16 @@ Import-Module -Name PowerShellGet -ErrorAction Stop
 Import-Module -Name PackageManagement -ErrorAction Stop
 
 #region Download Require Azure Modules
-If ($Version -ge '1910')
+If ($Version -ge '2002')
+{
+    Write-Host "Downloading AzureRM Package"
+    Save-Package -ProviderName NuGet -Source https://www.powershellgallery.com/api/v2 -Name AzureRM -Path $Path -Force -RequiredVersion 2.5.0
+
+    Write-Host "Downloading AzureStack Package"
+    Save-Package -ProviderName NuGet -Source https://www.powershellgallery.com/api/v2 -Name AzureStack -Path $Path -Force -RequiredVersion 1.8.1
+}
+
+If ($Version -eq '1910')
 {
     Write-Host "Downloading AzureRM Package"
     Save-Package -ProviderName NuGet -Source https://www.powershellgallery.com/api/v2 -Name AzureRM -Path $Path -Force -RequiredVersion 2.5.0

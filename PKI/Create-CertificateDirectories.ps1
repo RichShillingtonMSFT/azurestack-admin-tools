689458f167ba404780cfad894f2a4087ecfd10ef
Rich Shillington <rishilli@microsoft.com>
2020-11-23 11:38:26 -0500

    Added script to create the cert directories

---
+++ PKI/Create-CertificateDirectories.ps1
@@ -0,0 +1,16 @@
+﻿# Deployment Folders
+$Destination = 'C:\Certificates\Deployment'
+New-Item 'C:\Certificates\Deployment' -ItemType Directory
+$Directories = 'ACSBlob', 'ACSQueue', 'ACSTable', 'Admin Extension Host', 'Admin Portal', 'ARM Admin', 'ARM Public', 'KeyVault', 'KeyVaultInternal', 'Public Extension Host', 'Public Portal'
+$Directories | % { New-Item -Path (Join-Path $Destination $PSITEM) -ItemType Directory -Force}
+
+# App Service Folders
+$AppServicesDirectory = 'C:\Certificates\AppServices'
+New-Item $AppServicesDirectory -ItemType Directory
+$AppServiceSubDirectories = 'DefaultDomain', 'Identity', 'API', 'Publishing'
+$AppServiceSubDirectories | % { New-Item -Path (Join-Path $AppServicesDirectory $PSITEM) -ItemType Directory -Force}
+
+# Other Folders
+$OtherFoldersDirectory = 'C:\Certificates\'
+$OtherFolders = 'DBAdapter','EventHubs','IoTHub'
+$OtherFolders | % { New-Item -Path (Join-Path $OtherFoldersDirectory $PSITEM) -ItemType Directory -Force}

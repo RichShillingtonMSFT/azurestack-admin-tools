64c2c73b3abb448daf84a8c47ff7422d6a693932
Rich Shillington <rich@shillington.tv>
2022-03-30 08:39:31 -0400

    Commit

---
+++ Operator Workstation Setup/Install-AzureStackTools - AzureRM.ps1
@@ -0,0 +1,16 @@
+# Change directory to the root directory.
+Set-Location 'C:\Program Files\WindowsPowerShell\Modules'
+
+# Download the tools archive.
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+invoke-webrequest `
+  https://github.com/Azure/AzureStack-Tools/archive/master.zip `
+  -OutFile master.zip
+
+# Expand the downloaded files.
+expand-archive master.zip `
+  -DestinationPath . `
+  -Force
+
+# Change to the tools directory.
+Set-Location AzureStack-Tools-master
